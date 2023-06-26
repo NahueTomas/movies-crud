3a231fd1ed7bcdaadf63c8442bfc8e024a26a9dd
Nahuel <nahuetomas@outlook.com>
2023-06-26 16:03:18 -0300

    [UPDATE] Response type

--- routes/movies.py
+++ routes/movies.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse, Response
 
 from db.database import db, Movies
 from models.movie import Movie, Id
@@ -10,30 +11,69 @@ movies = APIRouter(
 )
 
 
-@movies.get('/')
-def get_all() -> Movies:
+@movies.get('/', response_model=Movies)
+def get_all():
     movies: Movies = db.get_all_movies()
-    return movies
+    # Movies to dict
+    movies_dict = []
+    for movie in movies:
+        movie_dict = {
+            "id": str(movie.id),
+            "title": movie.title,
+            "release": movie.release,
+            "rated": movie.rated,
+            "image": movie.image
+        }
+        movies_dict.append(movie_dict)
+    return JSONResponse(content=movies_dict, status_code=status.HTTP_200_OK)
 
 
-@movies.get('/{id}')
-def get_movie(id: Id) -> Movie | None:
+@movies.get('/{id}', response_model=Movie)
+def get_movie(id: Id):
     movie: Movie | None = db.get_movie(id)
-    return movie
+    if not (movie):
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    movie_dict = {
+        "id": str(movie.id),
+        "title": movie.title,
+        "release": movie.release,
+        "rated": movie.rated,
+        "image": movie.image
+    }
+    return JSONResponse(content=movie_dict, status_code=status.HTTP_200_OK)
 
 
-@movies.post('/')
-def create_movie(movie: MovieCreate) -> Movie:
+@movies.post('/', response_model=Movie)
+def create_movie(movie: MovieCreate):
     new_movie: Movie | None = db.create_movie(
         title=movie.title,
         release=movie.release,
         rated=movie.rated,
         image=movie.image,
     )
-    return new_movie
+
+    movie_dict = {
+        "id": str(new_movie.id),
+        "title": new_movie.title,
+        "release": new_movie.release,
+        "rated": new_movie.rated,
+        "image": new_movie.image
+    }
+    return JSONResponse(content=movie_dict, status_code=status.HTTP_201_CREATED)
 
 
-@movies.put('/{id}')
-def update_movie(movie: MoviePut, id: Id) -> Movie | None:
+@movies.put('/{id}', response_model=Movie)
+def update_movie(movie: MoviePut, id: Id):
     updated_movie: Movie | None = db.update_movie(movie, id)
-    return updated_movie
+    if not (updated_movie):
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    movie_dict = {
+        "id": str(updated_movie.id),
+        "title": updated_movie.title,
+        "release": updated_movie.release,
+        "rated": updated_movie.rated,
+        "image": updated_movie.image
+    }
+    return JSONResponse(content=movie_dict, status_code=status.HTTP_200_OK)
